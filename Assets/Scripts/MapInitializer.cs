d5b1d030507959d9972a140d4eb27c417812b3b2
Malcolm Hays <mah2005atM@gmail.com>
2025-12-28 14:42:28 -0800

    Add map initalizer script

---
+++ Assets/Scripts/MapInitializer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MapBuilder
+{
+    public class MapInitializer : MonoBehaviour
+    {
+        [SerializeField] private Map map;
+
+        void Awake()
+        {
+            
+        }
+    }
+}
