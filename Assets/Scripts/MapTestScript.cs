8e76279d358bd8671bd32d23a64776f29af583b9
Malcolm Hays <mah2005atM@gmail.com>
2025-12-29 15:15:53 -0800

    Update TestMap

--- Assets/Scripts/MapTestScript.cs
+++ Assets/Scripts/MapTestScript.cs
@@ -6,21 +6,22 @@ public class MapTestScript : MonoBehaviour
 {
     void Awake()
     {
-        // Vector2Int occupiedSpace1 = new Vector2Int(0, 0);
-        // List<Vector2Int> occupiedSpaces = new List<Vector2Int>();
+        // Vector3Int occupiedSpace1 = new Vector3Int(0, 0, 0);
+        // List<Vector3Int> occupiedSpaces = new List<Vector3Int>();
         // occupiedSpaces.Add(occupiedSpace1);
 
         // Piece testPiece = new Piece("floor", occupiedSpaces);
-        // MapPiece testMapPiece1 = new MapPiece(new Vector2Int(0, 0), testPiece, 0);
-        // MapPiece testMapPiece2 = new MapPiece(new Vector2Int(0, 1), testPiece, 0);
+        // MapPiece testMapPiece1 = new MapPiece(new Vector3Int(0, 0, 0), testPiece, 0);
+        // MapPiece testMapPiece2 = new MapPiece(new Vector3Int(0, 0, 1), testPiece, 0);
         // 
         // List<MapPiece> testMapPieces = new List<MapPiece>();
         // testMapPieces.Add(testMapPiece1);
         // testMapPieces.Add(testMapPiece2);
 
-        // Map testMap = new Map();
+        // Map testMap = new Map(6.4f, testMapPieces);
 
         MapFileStorage mapFileStorage = new MapFileStorage();
+        // mapFileStorage.WriteMapToFile(testMap, "TestMap");
 
         Map mapFromFile = mapFileStorage.ReadMapFromFile("TestMap");
 
