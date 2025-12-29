53de622c7abed3ebec4eb8c8d166001ee8c6b847
Malcolm Hays <mah2005atM@gmail.com>
2025-12-29 15:51:17 -0800

    Start writing map editor script

---
+++ Assets/Scripts/MapEditor.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+namespace MapBuilder
+{
+    public class MapEditor : MonoBehaviour
+    {
+        private List<string> keys = new List<string> {
+            "wall",
+            "ceiling",
+            "floor"
+        };
+
+        private Dictionary<string, MapPiece> mapPieceDictionary;
+        private Dictionary<string, AsyncOperationHandle<GameObject>> operationDictionary;
+        public UnityEvent Ready;
+
+        private bool assetsLoaded = false;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            Ready.AddListener(OnAssetsReady);
+        }
+
+        private void OnAssetsReady()
+        {
+            assetsLoaded = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (assetsLoaded)
+            {
+
+            }
+        }
+
+        // From: https://docs.unity3d.com/Packages/com.unity.addressables@1.19/manual/LoadingAddressableAssets.html#correlating-loaded-assets-to-their-keys
+        IEnumerator LoadAndAssociateResultWithKey(IList<string> keys) {
+            if (operationDictionary == null)
+                operationDictionary = new Dictionary<string, AsyncOperationHandle<GameObject>>();
+
+            AsyncOperationHandle<IList<IResourceLocation>> locations
+                = Addressables.LoadResourceLocationsAsync(keys,
+                    Addressables.MergeMode.Union, typeof(GameObject));
+
+            yield return locations;
+
+            var loadOps = new List<AsyncOperationHandle>(locations.Result.Count);
+
+            foreach (IResourceLocation location in locations.Result) {
+                AsyncOperationHandle<GameObject> handle =
+                    Addressables.LoadAssetAsync<GameObject>(location);
+                handle.Completed += obj => operationDictionary.Add(location.PrimaryKey, obj);
+                loadOps.Add(handle);
+            }
+
+            yield return Addressables.ResourceManager.CreateGenericGroupOperation(loadOps, true);
+
+            Ready.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var item in operationDictionary) {
+                Addressables.Release(item.Value);
+            }
+        }
+    }
+}
