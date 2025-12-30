37f4e09861be0cf4e5c731d53d3c4c7d00020441
Malcolm Hays <mah2005atM@gmail.com>
2025-12-30 11:33:56 -0800

    Make preview able to be rotated

--- Assets/Scripts/EditorPiecePreview.cs
+++ Assets/Scripts/EditorPiecePreview.cs
@@ -10,7 +10,7 @@ namespace MapBuilder
         bool pieceInstantiated = false;
 
         [SerializeField] private float reachDistance = 10f;
-        private float gridUnitSize;
+        private float gridUnitSize = 6.4f;
 
         [SerializeField] private CreativePlayerCamera cameraScript;
         
@@ -29,6 +29,7 @@ namespace MapBuilder
             if (!pieceInstantiated)
             {
                 piecePrefab = MapEditor.Instance.mapPiecePrefabs["wall"];
+                piece = new MapPiece(new Vector3Int(0, 0, 0), new Piece("wall"), 0);
                 pieceObject = Instantiate(piecePrefab);
                 pieceInstantiated = true;
             }
@@ -41,12 +42,14 @@ namespace MapBuilder
                         Mathf.RoundToInt(targetPosition.y / gridUnitSize),
                         Mathf.RoundToInt(targetPosition.z / gridUnitSize));
 
+                piece.location = newGridPosition;
                 pieceObject.transform.position = new Vector3(newGridPosition.x * gridUnitSize, newGridPosition.y * gridUnitSize, newGridPosition.z * gridUnitSize);
             }
 
             if (MapEditorInputManager.Instance.rotateAction.inProgress && alreadyRotated == false)
             {
                 alreadyRotated = true;
+                piece.orientation++;
                 Quaternion newRotation = new Quaternion();
                 newRotation.eulerAngles = new Vector3(0, piece.orientation * 90, 0);
                 pieceObject.transform.rotation = newRotation;
