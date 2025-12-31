4d65e827206a3460d0f00e6a5547c2abc6d95c27
Malcolm Hays <mah2005atM@gmail.com>
2025-12-31 09:53:08 -0800

    Fix a problem with the scripts and add better model

--- Assets/CreativePlayerMovement.cs
+++ Assets/CreativePlayerMovement.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 
-public class CreativePlayerMovement : MonoBehaviour
+namespace MapBuilder
 {
-    [SerializeField] private float _moveSpeed = 7f;
-    [SerializeField] private float _fastSpeed = 20f;
-
-    private Vector3 _velocity;
-
-    void Update()
+    public class CreativePlayerMovement : MonoBehaviour
     {
-        float speed = MapEditorInputManager.Instance.fastAction.inProgress ? _fastSpeed : _moveSpeed;
+        [SerializeField] private float _moveSpeed = 7f;
+        [SerializeField] private float _fastSpeed = 20f;
 
-        _velocity = (transform.forward * MapEditorInputManager.Instance.moveDirection.y + transform.right * MapEditorInputManager.Instance.moveDirection.x) * speed;
-        
-        if (MapEditorInputManager.Instance.upAction.inProgress)
-        {
-            _velocity.y = speed;
-        }
-        else if (MapEditorInputManager.Instance.downAction.inProgress)
-        {
-            _velocity.y = -speed;
-        }
+        private Vector3 _velocity;
 
-        transform.position += _velocity * Time.deltaTime;
+        void Update()
+        {
+            float speed = MapEditorInputManager.Instance.fastAction.inProgress ? _fastSpeed : _moveSpeed;
+
+            _velocity = (transform.forward * MapEditorInputManager.Instance.moveDirection.y + transform.right * MapEditorInputManager.Instance.moveDirection.x) * speed;
+            
+            if (MapEditorInputManager.Instance.upAction.inProgress)
+            {
+                _velocity.y = speed;
+            }
+            else if (MapEditorInputManager.Instance.downAction.inProgress)
+            {
+                _velocity.y = -speed;
+            }
+
+            transform.position += _velocity * Time.deltaTime;
+        }
     }
 }
