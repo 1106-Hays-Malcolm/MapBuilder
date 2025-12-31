b7a7aacafc05fd37e48489bf3e3d471c92453acb
Malcolm Hays <mah2005atM@gmail.com>
2025-12-31 11:47:56 -0800

    Add minecraft-like block placing

--- Assets/Scripts/EditorPiecePreview.cs
+++ Assets/Scripts/EditorPiecePreview.cs
@@ -40,12 +40,11 @@ namespace MapBuilder
                 pieceInstantiated = true;
             }
 
+            // GetPlaceGridPosition(100f, cameraScript.playerCamera.transform.forward, cameraScript.playerCamera.transform.position);
 
             Vector3 floatingTargetPosition = transform.position + (reachDistance * cameraScript.playerCamera.transform.forward);
-            Vector3Int newGridPosition = new Vector3Int(
-                    Mathf.RoundToInt(floatingTargetPosition.x / gridUnitSize),
-                    Mathf.RoundToInt(floatingTargetPosition.y / gridUnitSize),
-                    Mathf.RoundToInt(floatingTargetPosition.z / gridUnitSize));
+            // Vector3Int newGridPosition = GetGridPositionFromWorldPosition(floatingTargetPosition);
+            Vector3Int newGridPosition = GetPlaceGridPosition(100f, reachDistance, cameraScript.playerCamera.transform.forward, cameraScript.playerCamera.transform.position);
 
             piece.location = newGridPosition;
             pieceObject.transform.position = new Vector3(newGridPosition.x * gridUnitSize, newGridPosition.y * gridUnitSize, newGridPosition.z * gridUnitSize);
@@ -76,6 +75,34 @@ namespace MapBuilder
             {
                 alreadyPlaced = false;
             }
+        }
+
+        private Vector3Int GetPlaceGridPosition(float maxHitDisance, float maxFloatingDistance, Vector3 forward, Vector3 origin)
+        {
+            RaycastHit hit;
+            Vector3Int normal;
+            Vector3Int gridPositionHit;
+
+            if (Physics.Raycast(origin, forward, out hit, maxHitDisance))
+            {
+                normal = new Vector3Int(Mathf.RoundToInt(hit.normal.x), Mathf.RoundToInt(hit.normal.y), Mathf.RoundToInt(hit.normal.z));
+                Vector3 positionHit = hit.collider.transform.position;
+                gridPositionHit = GetGridPositionFromWorldPosition(positionHit);
+                gridPositionHit.y--;
+                return gridPositionHit + normal;
+            }
+            else
+            {
+                return GetGridPositionFromWorldPosition(origin + (maxFloatingDistance * forward));
+            }
+        }
+
+        private Vector3Int GetGridPositionFromWorldPosition(Vector3 position)
+        {
+            return new Vector3Int(
+                    Mathf.RoundToInt(position.x / gridUnitSize),
+                    Mathf.RoundToInt(position.y / gridUnitSize),
+                    Mathf.RoundToInt(position.z / gridUnitSize));
         }
     }
 }
