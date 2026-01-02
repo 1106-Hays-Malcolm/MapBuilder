b966bca5fcb6993bda1d11fabdc1b730b850ebdf
Malcolm Hays <mah2005atM@gmail.com>
2026-01-01 22:04:33 -0800

    Remove unused "occupied spaces"

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -8,27 +8,16 @@ namespace MapBuilder
     public class Piece
     {
         [SerializeField] private string _prefabName;
-        [SerializeField] private List<Vector3Int> _occupiedSpaces;
-
         public string prefabName { get => _prefabName; }
-        public List<Vector3Int> occupiedSpaces { get => _occupiedSpaces; }
-
-        public Piece(string newPrefabName, List<Vector3Int> newOccupiedSpaces)
-        {
-            _prefabName = newPrefabName;
-            _occupiedSpaces = newOccupiedSpaces;
-        }
 
         public Piece(string newPrefabName)
         {
             _prefabName = newPrefabName;
-            _occupiedSpaces = new List<Vector3Int> { new Vector3Int(0, 0, 0), };
         }
 
         public Piece()
         {
             _prefabName = "default";
-            _occupiedSpaces = new List<Vector3Int> { new Vector3Int(0, 0, 0), };
         }
     }
 }
